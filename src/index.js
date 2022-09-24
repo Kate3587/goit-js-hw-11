5b3c5e9ebc9c698072a98347702827a3c05e19b7
Kate3587 <108617406+Kate3587@users.noreply.github.com>
2022-09-25 01:26:56 +0200

    task

--- src/index.js
+++ src/index.js
@@ -10,19 +10,16 @@ const formEl = document.querySelector('#search-form');
 const moreBtn = document.querySelector('.load-more')
 
 let page = 1;
+let searchValue = '';
 const totalPages = Math.ceil(500 / itemPerPage);
 
 formEl.addEventListener('submit', onSubmit);
-
-
 
 async function loadMorePhoto(searchValue) {
   page += 1;
   const data = await getPhoto(searchValue, page);
   createGalleryMarkup(data.hits);
-  // data.hits.forEach(photo => {
-  //   createCardMurkup(photo);
-  // });
+
   if (page === totalPages) {
     addClass('visually-hidden');
   };
@@ -31,31 +28,26 @@ async function loadMorePhoto(searchValue) {
     try {
       const data = await getPhoto(searchValue, page);
       removeClass('visually-hidden');
+      moreBtn.removeEventListener('click', listenerCallback)
+      moreBtn.addEventListener('click', listenerCallback)
       
-      const moreBtnCallback = () => {
-        loadMorePhoto(searchValue)
-      }
-      
-      moreBtn.removeEventListener('click', moreBtnCallback);
-      
-      moreBtn.addEventListener('click', moreBtnCallback);
-
       if (data.hits.length === 0) {
-        addClass('visually-hidden');
         Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      } else 
+      } else {
         Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
-       
-        createGalleryMarkup(data.hits);
-        // data.hits.forEach(photo => {
-        //   createCardMurkup(photo);
-        // });
+      }
+      createGalleryMarkup(data.hits);
         createLightbox();
     } catch (error) {
       addClass('visually-hidden');
       console.log(error);
     };
-  };
+};
+  
+function listenerCallback() {
+  loadMorePhoto(searchValue);
+}
+
 function createGalleryMarkup(cardsArr) {
   const markUp = cardsArr.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `<div class="photo-card">
   <a class="link-img" href=${largeImageURL}><img class="card-img" src=${webformatURL} alt=${tags} loading="lazy" /></a>
@@ -78,33 +70,15 @@ function createGalleryMarkup(cardsArr) {
   galleryEl.insertAdjacentHTML('beforeend', markUp);
 }
 
-
-
-// function createCardMurkup({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) {
-//     galleryEl.insertAdjacentHTML('beforeend', `<div class="photo-card">
-//   <a class="link-img" href=${largeImageURL}><img class="card-img" src=${webformatURL} alt=${tags} loading="lazy" /></a>
-//   <div class="info">
-//     <p class="info-item">
-//       <b class="info-item-label">Likes: </b><span class="info-item-span">${likes}</span>
-//     </p>
-//     <p class="info-item">
-//       <b class="info-item-label">Views: </b><span class="info-item-span">${views}</span>
-//     </p>
-//     <p class="info-item">
-//       <b class="info-item-label">Comments: </b><span class="info-item-span">${comments}</span>
-//     </p>
-//     <p class="info-item">
-//       <b class="info-item-label">Downloads: </b><span class="info-item-span">${downloads}</span>
-//     </p>
-//   </div>
-// </div>`);
-//   };
-
    function onSubmit (event) {
     event.preventDefault();
     removeMarkup(galleryEl);
 
-    const searchValue = event.currentTarget[0].value;
+     searchValue = event.currentTarget[0].value;
+     
+     if (searchValue === '') {
+       Notiflix.Notify.info('Write something.')
+     }
     mountData(searchValue);
 }
 
@@ -118,7 +92,6 @@ function createGalleryMarkup(cardsArr) {
 
     let lightbox = new SimpleLightbox('.photo-card a', { captionDelay: 300 })
   }
-
 
 
 function removeMarkup(element) {
